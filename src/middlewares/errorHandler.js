968593e8df506e2fc06767a4a1d5ffc9f3d4d90f
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-21 14:56:34 -0300

    configuring the errorHandler.js file

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,15 @@
+const errorHandler = (err, req, res, next) => {
+    console.error(err.stack);
+
+    const statusCode = err.statusCode || 500;
+
+    const message = err.message || 'An unexpected error occurred on the server.';
+
+    res.status(statusCode).json({
+        stats: 'error',
+        statusCode,
+        message
+    });
+};
+
+module.exports = errorHandler;
